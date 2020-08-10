2f26614ffe96782be9727a2a9224dd00498f53b4
trọng hoàng ngọc <gnort97@gmail.com>
2020-08-10 23:17:48 +0700

    ..

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,324 +1,162 @@
-<!DOCTYPE html>
-<html lang="en">
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
+    <script type="text/javascript" src="http://www.maytinhhtl.com/images/code/snowstorm.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">	
+<title>I Love You :-*</title>
+<link rel="stylesheet" href="http://yeuemvv.epizy.com/css/popup.css">
+<link rel="stylesheet" href="http://yeuemvv.epizy.com/css/styles.css">
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro&subset=latin,vietnamese' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.min.js"></script>
+<script type="text/javascript" src="js/jquery.popup.js"></script>
+<script src="js/popup.js" type="text/javascript"></script>
+<script src="js/jquery-1.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="http://cdn.vietdesigner.net/data/codes/snow1.js"></script>
 <style>
-/*Custom fonts - Sniglet and FontAwesome*/
-@import url('https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap');
-.cursive {
-    font-family: 'Patrick Hand', cursive;
-}
-
-* {margin: 0; padding: 0;}
-body {
-    background: hsl(120, 40%, 60%);
-    padding-top: 150px;
-    font: normal 18px cursive; color: white; text-align: center;
-}
-
-/*general styles*/
-h1 {font-weight: normal; font-size: 36px; margin-bottom: 75px;}
-.fun-cube i {transform: scale(10); opacity: 0.1;}
-
-#cuboid {
-    width: 400px; margin: 0 auto;
-    /*this also makes #cuboid a container for absolutely positioned descendants*/
-    perspective: 1000px;
-}
-#cuboid form {
-    /*counter translate*/
-    transform: translateZ(-20px);
-    /*propogate 3d space for children*/
-    transform-style: preserve-3d;
-    /*prevent height collapse as children are absolutely positioned*/
-    height: 40px;
-    /*for smooth animations*/
-    transition: all 0.35s;
-}
-
-/*faces*/
-.loader {
-    background: hsl(120, 40%, 30%);
-    animation: phase 1s infinite;
-}
-/*Lets create a pulsating animation for the loader face*/
-@keyframes phase {
-    50% {background: hsl(120, 70%, 30%);}
-}
-#name_text {
-    background: white; outline: none; border: 0 none;
-    font: unset; text-align: left; color: hsl(120, 40%, 30%);
-    display: block; width: 100%;min-height:50px;  padding: 0 10px;border-radius:20px;
-    box-sizing: border-box;
-}
-#submit {display: none;}
-
-.submit-icon, .reset-icon {
-    position: absolute; top: 0; right: 0;
-    color: rgba(0, 0, 0, 0.25);
-    line-height: 40px; padding: 0 10px;
-    /*smooth transitions when user activates input and types something*/
-    transition: all 0.5s;
-    /*to make the icons feel like buttons*/
-    cursor: pointer;
-}
-/*.active = when the user is typing something*/
-.submit-icon.active {color: hsl(120, 40%, 30%);}
-.reset-icon {color: rgba(255, 255, 255, 0.25); font-size: 14px;}
-
-
-
-/*the form will have 4 states/classes(default+3) for rotation*/
-.success{
-    width: fit-content;
-    min-width: 110px;
-    height: 40px;
-    border-radius: 40px;
-    border: none;
-    background: #1F471f;
-}
-.inline{
-    display: inline-block!important;
-}
-.dp_none{
-    display: none;
-}
-.loan_bg{
-
-    background: azure!important;
-}
+    body{
+        background-image: url(https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-9/51369897_2264155860491942_4455491999335710720_o.jpg?_nc_cat=101&_nc_sid=0be424&_nc_ohc=c14QJlB9sTYAX9eCWIK&_nc_ht=scontent.fhan2-1.fna&oh=ce0edc567339875cc4171b4b34de86e4&oe=5F58C0EF);
+    }
 </style>
-</head>
-<body>
-<div id="video" class="dp_none">
-<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fgnort1997%2Fvideos%2F185201049326439%2F&show_text=0&width=267"  style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
-</div>
-<div class="fun-cube"><i class="fa fa-cube"></i></div>
-<h1 id="alert_form">Xin chào bạn "gì" xinh đẹp <span style='font-size:30px;'>&#128536;</span></h1>
-<div id="cuboid">
-    <form>
-        <!-- #1 hover button -->
-        <!-- <div>
-            <p id="text1" class="cuboid-text">Di con chuột vô đây đi má &#x1F602;</p>
-        </div> -->
-        <!-- #2 text input -->
-        <div>
-            <input type="text" id="name_text" class="cuboid-text" placeholder="Mình Xin tên bạn đi  &ensp; &#x1F60D; &#x1F60D;" autocomplete="off"/>
-            <!-- hidden submit button -->
-        </div>
-    </form>
-</div>
-<div id="so1" style="padding-top: 20px;display: inline-block">
-    <div class="success"><span style='font-size:20px;position: relative;top: 5px;'>&#128076;</span></div>
-</div>
+<script type="text/javascript">
 
-<div id="so2" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>được chớ</span></div>
-</div>
-
-<div id="so3" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>hông nha</span></div>
-</div>
-
-<div id="so4" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>chưa nhé</span></div>
-</div>
-
-<div id="so5" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>đúng rồi á</span></div>
-</div>
-
-<div id="so6" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>ừa</span></div>
-</div>
-
-<div id="so7" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>thôi đại đi</span></div>
-</div>
-
-<div id="so8" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>thì sao , kệ tui &#128548;</span></div>
-</div>
-
-<div id="so9" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>thì kệ &#128518;</span></div>
-</div>
-
-<div id="so10" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>tui khó cua lắm á &#128517;</span></div>
-</div>
-<div id="so11" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>có gì mà đòi cưa tui chứ &#x1F61D;</span></div>
-</div>
-
-<div id="so12" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>hông thèm nha</span></div>
-</div>
-
-<div id="so13" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>thử đi coi nào</span></div>
-</div>
-
-<div id="so14" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>được rồi , nhây vừa vừa &#128545;</span></div>
-</div>
-<div id="so15" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>đi nhậu </span></div>
-</div>
-<div id="so16" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>đi lượn lờ cũng được</span></div>
-</div>
-<div id="so17" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>đi ăn </span></div>
-</div>
-<div id="so18" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>đi xem phim </span></div>
-</div>
-<div id="so19" style="padding-top: 20px;display: none">
-    <div class="success"><span style='font-size:18px;position: relative;top: 5px;'>biến , biến , biếnnnn </span></div>
-</div>
-<!-- jQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</body>
-<script>
-    var data = {name:"",interests:""};
-
-$('#so1').click(function(){
-   var name = $('#name_text').val();
-   data.name = name;
-    if (name ==="loan" || name === "Loan" || name === "Mai Thanh Loan"|| name === "mai thanh loan"|| name === "MAI THANH LOAN"|| name === "LOAN"){
-       $("body").css({"background-image": "url(\"https://scontent.fhan5-5.fna.fbcdn.net/v/t1.0-9/25659645_2016871718553692_4648996418019014365_n.jpg?_nc_cat=101&_nc_sid=dd9801&_nc_ohc=FFnb-YUGxagAX8Pt0yw&_nc_ht=scontent.fhan5-5.fna&oh=e3db8d5b9d9d8a05572fc013f0f3f43a&oe=5F52790E\")",
-                    "background-position":"center",
-                    "background-repeat":"no-repeat",
-       });
-       $("#alert_form").html("Bạn " +name+" xinh đẹp bạn cho mình làm quen được hônggggggg<span style='font-size:30px;'>&#128536;</span>");
-       $("#so1").toggle();
-       $("#text1").toggle();
-       $("#name_text").toggle();
-       $("#so2").addClass("inline");
-       $("#so3").addClass("inline");
-   }
-   else {
-       $("#alert_form").html("Chúc bạn một ngày vui vẻ <span style='font-size:30px;'>&#128516;</span>");
-       $("#text1").toggle();
-       $("#so1").toggle();
-       $("#name_text").toggle();
-   }
+$(document).ready(function () {
+	$('a.btn-ok, #msb-nt').click(function () {		
+		$('#dialog-overlay, #dialog-box').hide();		
+		return false;
+	});
+	$(window).resize(function () {
+		if (!$('#dialog-box').is(':hidden')) popup();		
+	});	
 });
-$('#so2').click(function(){
-    var name = data.name;
-    $("#alert_form").html("Bạn " +name+" đáng yêu vậy chắc có người yêu rồi ha <span style='font-size:30px;'>&#128554;</span>");
-    $("#so2").removeClass("inline");
-    $("#so3").removeClass("inline");
-    $("#so4").addClass("inline");
-    $("#so5").addClass("inline");
-});
-$('#so3').click(function(){
-    $("#alert_form").html("Quen thì quen mà hông quen thì quen  , thì thôi quen đại đi mà ....<span style='font-size:30px;'>&#128554;</span>");
-    $("#so2").removeClass("inline");
-    $("#so3").removeClass("inline");
-    $("#so6").addClass("inline");
-    $("#so7").addClass("inline");
-});
-$('#so4').click(function(){
-    $("#alert_form").html("Xinh vậy chưa có người yêu , Bà còn đợi ai nữa <span style='font-size:30px;'>&#x1F644;</span> Hay bà thích con gái ta <span style='font-size:30px;'>&#128563;</span>");
-    $("#so2").removeClass("inline");
-    $("#so3").removeClass("inline");
-    $("#so4").removeClass("inline");
-    $("#so5").removeClass("inline");
-    $("#so8").addClass("inline");
-
-});
-$('#so8').click(function(){
-    var name = data.name;
-    $("#alert_form").html(" Nhưng tui lỡ thích "+ name+ " rồi thì sao giờ <span style='font-size:30px;'>&#128534;</span>");
-    $("#so8").removeClass("inline");
-    $("#so9").addClass("inline");
-    $("#so10").addClass("inline");
-
-});
-$('#so9').click(function(){
-    var name = data.name;
-    $("#alert_form").html("Tui thích "+ name+ " thật á <span style='font-size:30px;'>&#128532;</span> cho tui cơ hội đi mà...... ");
-    $("#so9").removeClass("inline");
-    $("#so10").removeClass("inline");
-    $("#so11").addClass("inline");
-
-});
-$('#so11').click(function(){
-    var name = data.name;
-    $("#alert_form").html("Tui cũng ngoan nè , thích " +name+"thật lòng nữa chớ <span style='font-size:30px;'>&#128525;</span> , tui còn biết hát á, nghe thử hông ....");
-    $("#so9").removeClass("inline");
-    $("#so10").removeClass("inline");
-    $("#so11").removeClass("inline");
-    $("#so12").addClass("inline");
-    $("#so13").addClass("inline");
-});
-$('#so11').click(function(){
-    var name = data.name;
-    $("#alert_form").html("Coi là đồng ý cho tui cua " +name+"<span style='font-size:30px;'>&#128525;</span> nha");
-    $("#so9").removeClass("inline");
-    $("#so10").removeClass("inline");
-    $("#so11").removeClass("inline");
-    $("#so12").addClass("inline");
-    $("#so13").addClass("inline");
-});
-$('#so13').click(function(){
-    var name = data.name;
-    $("#alert_form").html("Coi là đồng ý cho tui cua " +name+"<span style='font-size:30px;'>&#128525;</span> nha");
-    $("#so9").removeClass("inline");
-    $("#so10").removeClass("inline");
-    $("#so11").removeClass("inline");
-    $("#so12").removeClass("inline");
-    $("#so13").removeClass("inline");
-    $("#video").removeClass("dp_none");
-
-});
-$("#so6,#so7").click(function(){
-    var name = data.name;
-    $("#alert_form").html("Nếu được , 'NẾU' thôi nha <span style='font-size:30px;'>&#128556;</span> thì tui rủ "+name+" đi chơi được hông ?????");
-    $("#so14").addClass("inline");
-    $("#so6").removeClass("inline");
-    $("#so7").removeClass("inline");
-});
-$("#so14").click(function(){
-    var name = data.name;
-    $("#alert_form").html("Đi đâu thì "+name+" thấy zui ???");
-    $("#so14").removeClass("inline");
-    $("#so15,#so16,#so17,#so18").addClass("inline");
-});
-$("#so15,#so16,#so17,#so18").click(function(){
-    var name = data.name;
-    $("#alert_form").html("Thôi đùa vậy chớ , ngồi nghĩ mấy cái kịch bản này mệt quá <span style='font-size:30px;'>&#128557;</span> mà a thích "+name+" thật chớ , cho a 'CUA' em thử đê, lỡ đâu em cũng thích anh thì sao <span style='font-size:30px;'>&#128540;</span> <span style='font-size:30px;'>&#128540;</span> <span style='font-size:30px;'>&#128540;</span>");
-    $("#so15,#so16,#so17,#so18").removeClass("inline");
-});
-$("#so5").click(function(){
-    var name = data.name;
-    $("#alert_form").html("Èo , mà người thế có người EO cũng đúng thôi <span style='font-size:30px;'>&#128542;</span> , nhưng mà kệ , tôi cứ cua đó , ai bắt bà gọi tui là BẤT CHẤP BOY  làm chi");
-    $("#so4,#so5").removeClass("inline");
-    $("#so19").addClass("inline");
-});
-$("#so19").click(function(){
-    var name = data.name;
-    $("#alert_form").html("Còn lâu mới biến nhé , bà có GẶM ĐẦU tui cũng hổng biến đấy , ờ ờ , tại tui thích bà thật mà , cho tui CUA đi mà <span style='font-size:30px;'>&#128549;</span> hứa ngoan ");
-
-    $("#so4,#so5").removeClass("inline");
-    $("#so19").removeClass("inline");
-});
-$("#so10").click(function(){
-    var name = data.name;
-    $("#alert_form").html("Mặc kệ , không cần biết khó gì hết , tui cứ CUA á , ai bắt để tui gặp "+name+" làm chi , không nói gì là đồng ý nha (tui có cho bà cái nút nào để bấm đâu mà chẳng hổng nói gì <span style='font-size:30px;'>&#128540;</span>)");
-
-    $("#so9,#so10").removeClass("inline");
-});
-$("#so12").click(function(){
-    var name = data.name;
-    $("#alert_form").html("Hỏng thèm cứ bắt coi đấy <span style='font-size:30px;'>&#128517;</span> mà đừng share nha, tui ngại chết ");
-
-    $("#so9,#so10").removeClass("inline");
-    $("#video").removeClass("dp_none");
-});
+function popup(message) {	
+	var maskHeight = $(document).height();  
+	var maskWidth = $(window).width();
+	var dialogTop =  (maskHeight/3) - ($('#dialog-box').height());  
+	var dialogLeft = (maskWidth/2) - ($('#dialog-box').width()/2); 
+	$('#dialog-overlay').css({height:maskHeight, width:maskWidth}).show();
+	$('#dialog-box').css({top:dialogTop, left:dialogLeft}).show();
+	$('#dialog-message').html(message);
+}
 </script>
+<script language="javascript"> 
+var kt=false
+function move()
+{
+var x = Math.random()*500
+var y = Math.random()*500
+var left=x+'px'
+var top=y+'px'
+document.getElementById('lbNo').style.left=left
+document.getElementById('lbNo').style.top=top
+}
+function dongy()
+{
+if(kt==false)
+{
+
+kt=true 
+} 
+else
+{
+}
+}
+</script>
+<script src="js/pace.min.js"></script>
+  <link href="css/dataurl.css" rel="stylesheet" />
+
+</head>	
+<body onload="popup('<p>Hế lô :)))</p></br>'); on()" onunload="s()">
+<!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v7.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      </div>
+<div id="bg"></div>
+<div id="myModal" class="reveal-modal large">
+    <div id="traloi">
+	  <p>Hello bạn Loan xinh đẹp</br></br></p>
+      <form id="f" name="f">
+        <input id="form" name="txt" type="text" />
+
+        <div>
+          <input id="gui" onclick="Yeu();" type="button" value="Gửi đi" />
+        </div>
+      </form>
+    </div>
+
+    <div style="clear: both;"></div>
+
+    <div id="divResult" style="text-align: center"></div>
+
+    <div id="divResult2"></div>
+
+    <div id="divResult3"></div>
+<a class="close-reveal-modal">&#215;</a>
+</div>
+<script language="javascript"> 
+    $(document).ready(function() {
+        $('#msb-nt').click(function() {
+                $('.music').slideToggle("fast");
+                $('#bg').css('display','none');
+        });
+    });
+</script>
+<div id="dialog-overlay"></div>
+<div id="dialog-box">
+	<div class="dialog-content">
+		<div id="dialog-message"></div>
+		<div id="msb-nt">
+		<a href="#">GÌ??</a>
+		</div>
+	</div>
+</div>
+		
+<div class="msg">
+			
+<h1>THÍCH TUI HÔNG ?</h1>
+			
+<h2>TẮT "TAB" NÀY ĐI LÀ CÓ :))))</h2>
+
+		
+</div>
+		
+<center>
+<div id="co"> 
+<a href="#" data-reveal-id="myModal">CÓ</a>
+</div> 
+
+<div id="lbNo" style="position:absolute; left: 700px; top: 300px;"> 
+<input type="button" value="KHÔNG" style="cursor:pointer;" onMouseMove="move()" > 
+</div>
+</center> 
+
+<div class="music" style="display: none;">
+<object width="1" height="1">  <param name="movie" value="http://www.nhaccuatui.com/m/BHKOQZhbiU" />  <param name="quality" value="high" />  <param name="wmode" value="transparent" />  <param name="allowscriptaccess" value="always" /> <param name="allowfullscreen" value="true"/> <param name="flashvars" value="autostart=true" />  <embed src="http://www.nhaccuatui.com/m/BHKOQZhbiU" flashvars="target=blank&autostart=true" allowscriptaccess="always" allowfullscreen="true" quality="high" wmode="transparent" type="application/x-shockwave-flash" width="1" height="1"></embed></object>
+</div>
+<script type="text/javascript">
+   $(document).ready(function(){
+      $(document).bind("contextmenu",function(e){
+        return false;
+      });
+    });
+ </script>	
+</body>
 </html>
